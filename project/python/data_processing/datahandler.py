18ea30ef882f6ebee5656b6fcbf5e1a224e2be34
yiliang_chen <chenyl9712@gmail.com>
2023-03-06 01:19:53 -0800

    dataloader.py: - load dataset and go through the preprocessing pipeline defined in helper class AudioMNIST - create dataloaders for training, validation, and test dataset
    datahandler.py:
    helper class AudioMNIST involves:
    - loading audio,
    - padding and shifting audio,
    - generating spectrogram of audio,
    - augmentation

---
+++ project/python/data_processing/datahandler.py
@@ -0,0 +1,64 @@
+import torchaudio
+import torch
+
+import random
+from torchaudio import transforms
+
+
+class AudioHandler:
+    @staticmethod
+    def open(file_path):
+        # throw away sample rate since it is a constant
+        signal, _ = torchaudio.load(file_path)
+        return signal
+
+    @staticmethod
+    def pad(signal, audio_length):
+        new_signal = torch.zeros((1, audio_length))
+        new_signal[0, 48000 - len(signal):] = signal
+
+        return new_signal
+
+    @staticmethod
+    def time_shift(signal, shift_limit):
+        """ Data augmentation: shift forward or back by a bit. """
+        signal_length = signal.shape[1]
+        shift = int(random.random() * shift_limit * signal_length)
+        return signal.roll(shift)
+
+    @staticmethod
+    def spectrogram(signal, n_mels=64, n_fft=2034, hop_len=None, mfcc=False, n_mfcc=40):
+        top_db = 80
+
+        spec = transforms.MelSpectrogram(sample_rate=48000, n_fft=n_fft,
+                                         hop_length=hop_len,
+                                         n_mels=n_mels)(signal)
+        spec = transforms.AmplitudeToDB(top_db=top_db)(torch.Tensor(spec))
+
+        if mfcc:
+            spec = transforms.MFCC(sample_rate=48000, n_mfcc=n_mfcc)
+
+        return spec
+
+    @staticmethod
+    def spectral_augmentation(spec, max_mask_ptc=0.1,
+                              n_freq_masks=1,
+                              n_time_masks=1):
+        """ Augment the spectrogram data. """
+        _, n_mels, n_steps = spec.shape
+        mask_value = spec.mean()
+        aug_spec = spec
+
+        freq_mask_param = max_mask_ptc * n_mels
+        for _ in range(n_freq_masks):
+            aug_spec = transforms.FrequencyMasking(
+                freq_mask_param
+            )(aug_spec, mask_value)
+
+        time_mask_param = max_mask_ptc * n_steps
+        for _ in range(n_time_masks):
+            aug_spec = transforms.TimeMasking(
+                time_mask_param
+            )(aug_spec, mask_value)
+
+        return aug_spec
---
+++ project/python/data_processing/dataloader.py
@@ -0,0 +1,82 @@
+import torch.utils.data
+from torch.utils.data import DataLoader, Dataset, random_split
+
+from data_processing.datahandler import AudioHandler
+
+
+class AudioMNIST(Dataset):
+    def __init__(self, datapath, load_entire_filetree=False):
+        self.files = self._build_files()
+        self.audio_len = 48000
+        self.shift_ptc = 0.4
+        self.datapath = datapath
+
+    def _build_files(self):
+        files = {}
+        index = 0
+        for ii in range(1, 61):
+            num = "0%d" % ii if ii < 10 else "%d" % ii
+            for jj in range(50):
+                for kk in range(10):
+                    files[index] = [
+                        self.datapath + num + "/%d_%s_%d.wav" % (
+                            kk, num, jj),
+                        kk
+                    ]
+                    index += 1
+
+        return files
+
+    def __len__(self):
+        return 30000
+
+    def __getitem__(self, idx):
+        signal = AudioHandler.open(self.files[idx][0])[0]
+        pad = AudioHandler.pad(signal, self.audio_len)
+        shift = AudioHandler.time_shift(pad, self.shift_ptc)
+        sgram = AudioHandler.spectrogram(
+            shift, n_mels=64, n_fft=1024, hop_len=None, mfcc=True
+        )
+        aug_sgram = AudioHandler.spectral_augmentation(
+            sgram, max_mask_ptc=0.1, n_freq_masks=2, n_time_masks=2
+        )
+
+        return aug_sgram, self.files[idx][1]
+
+
+class DataParam:
+    def __init__(self, ratio, batch_size, shuffle):
+        """
+        @param ratio: the ratio of the length of subset of data to the dataset
+        @param batch_size: size of the batch
+        @param shuffle: T/F
+        """
+        self.ratio = ratio
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+
+class LoaderCreator:
+    def __init__(self, datapath, num_workers=2):
+        """
+        @param datapath: the path to dataset
+        @param num_workers:
+        """
+        self.num_workers = num_workers
+        self.dataset = AudioMNIST(datapath)
+
+    def create_loaders(self, train_param: DataParam, val_param: DataParam, test_param: DataParam):
+        train_ds, val_ds, test_ds = random_split(self.dataset, [train_param.ratio, val_param.ratio, test_param.ratio])
+        train_dl = DataLoader(train_ds,
+                              batch_size=train_param.batch_size,
+                              shuffle=train_param.shuffle,
+                              num_workers=self.num_workers)
+        val_dl = DataLoader(val_ds,
+                            batch_size=val_param.batch_size,
+                            shuffle=val_param.shuffle,
+                            num_workers=self.num_workers)
+        test_dl = DataLoader(test_ds,
+                             batch_size=test_param.batch_size,
+                             shuffle=test_param.shuffle,
+                             num_workers=self.num_workers)
+        return train_dl, val_dl, test_dl
