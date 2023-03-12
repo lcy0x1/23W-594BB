f7c1a689a67fc803d4810af218c4c25daf45f6b7
lcy0x1 <arthurlcy0x1@gmail.com>
2023-03-11 17:00:34 -0800

    fix

--- project/verilog/data_io.v
+++ project/verilog/data_io.v
@@ -1,24 +1,22 @@
 module data_io(
     input clk,
     input rstn,
-    input [15:0] data_in,
-    output [15:0] data_out
+    input [22:0] data_in,
+    output [9:0] data_out
 );
 
-wire opcode_i = data_in[15:14];
-wire parity_i = data_in[11:10]; // high bit: write enable, low bit: 1/0 = msb/lsb
-wire indata_i = data_in[9:0];
+wire opcode_i = data_in[22:21];
+wire parity_i = data_in[20:19]; // high bit: write enable, low bit: 1/0 = msb/lsb
+wire indata_i = data_in[18:0];
 
 reg [1:0] data_parity;
 reg [18:0] acc_data;
 reg data_valid;
 
-wire [1:0] opcode = data_valid ? opcode_i : 0;
-
 wire [6:0] result;
 wire [1:0] status;
 
-assign data_out = {7'b0, status, result};
+assign data_out = {status, result};
 
 wrapper uut(clk, rstn, opcode, acc_data, result, status);
 
@@ -31,13 +29,8 @@ always @(posedge clk) begin
         if(data_parity != parity_i) begin
             data_parity <= parity_i;
             if(parity_i[1]) begin
-                if(parity_i[0]) begin
-                    acc_data[9:0] <= indata_i;
-                    data_valid <= 0;
-                end else begin
-                    acc_data[18:10] <= indata_i[8:0];
-                    data_valid <= 1;
-                end 
+                acc_data <= indata_i;
+                data_valid <= 1;
             end else begin
                 data_valid <= 0;
             end
